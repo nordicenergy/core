935c78b5130a86faaf7e90803491ee53679cd523
Brian Faust <hello@basecode.sh>
2020-11-11 05:00:55 +0200

    wip

--- packages/core-forger/src/delegate-tracker.ts
+++ packages/core-forger/src/delegate-tracker.ts
@@ -1,5 +1,5 @@
 import { Container, Contracts, Services, Utils } from "@arkecosystem/core-kernel";
-import { Managers, Utils as CryptoUtils } from "@arkecosystem/crypto";
+// import { Managers, Utils as CryptoUtils } from "@arkecosystem/crypto";
 
 import { Delegate } from "./interfaces";
 
@@ -33,14 +33,14 @@ export class DelegateTracker {
     @Container.inject(Container.Identifiers.BlockchainService)
     private readonly blockchainService!: Contracts.Blockchain.Blockchain;
 
-    /**
-     * @private
-     * @type {Contracts.State.WalletRepository}
-     * @memberof DelegateTracker
-     */
-    @Container.inject(Container.Identifiers.WalletRepository)
-    @Container.tagged("state", "blockchain")
-    private readonly walletRepository!: Contracts.State.WalletRepository;
+    // /**
+    //  * @private
+    //  * @type {Contracts.State.WalletRepository}
+    //  * @memberof DelegateTracker
+    //  */
+    // @Container.inject(Container.Identifiers.WalletRepository)
+    // @Container.tagged("state", "blockchain")
+    // private readonly walletRepository!: Contracts.State.WalletRepository;
 
     /**
      * @private
@@ -67,8 +67,8 @@ export class DelegateTracker {
     public async handle(): Promise<void> {
         // Arrange...
         const { height, timestamp } = this.blockchainService.getLastBlock().data;
-        const maxDelegates = Managers.configManager.getMilestone(height).activeDelegates;
-        const blockTime: number = CryptoUtils.calculateBlockTime(height);
+        // const maxDelegates = Managers.configManager.getMilestone(height).activeDelegates;
+        // const blockTime: number = CryptoUtils.calculateBlockTime(height);
         const round: Contracts.Shared.RoundInfo = Utils.roundCalculator.calculateRound(height);
 
         const activeDelegates = (await this.app
@@ -88,33 +88,16 @@ export class DelegateTracker {
             blockTimeLookup,
         );
 
-        this.logger.debug(JSON.stringify({
-            method: 'forgingInfo',
-            result: forgingInfo
-        }));
+        // // Determine Next Forgers...
+        // const nextForgers: string[] = [];
+        // for (let i = 0; i <= maxDelegates; i++) {
+        //     const delegate: string | undefined =
+        //         activeDelegatesPublicKeys[(forgingInfo.currentForger + i) % maxDelegates];
 
-        // @ts-ignore
-        this.logger.warning(JSON.stringify({
-            method: 'activeDelegatesPublicKeys',
-            result: {
-                height,
-                timestamp,
-                round,
-                activeDelegatesPublicKeys
-            }
-        }, null, 4))
-
-
-        // Determine Next Forgers...
-        const nextForgers: string[] = [];
-        for (let i = 0; i <= maxDelegates; i++) {
-            const delegate: string | undefined =
-                activeDelegatesPublicKeys[(forgingInfo.currentForger + i) % maxDelegates];
-
-            if (delegate) {
-                nextForgers.push(delegate);
-            }
-        }
+        //     if (delegate) {
+        //         nextForgers.push(delegate);
+        //     }
+        // }
 
         // // @ts-ignore
         // this.logger.warning(JSON.stringify({
@@ -126,57 +109,101 @@ export class DelegateTracker {
         //     }
         // }, null, 4))
 
-        if (activeDelegatesPublicKeys.length < maxDelegates) {
-            return this.logger.warning(
-                `Tracker only has ${Utils.pluralize(
-                    "active delegate",
-                    activeDelegatesPublicKeys.length,
-                    true,
-                )} from a required ${maxDelegates}`,
-            );
-        }
+        // if (activeDelegatesPublicKeys.length < maxDelegates) {
+        //     return this.logger.warning(
+        //         `Tracker only has ${Utils.pluralize(
+        //             "active delegate",
+        //             activeDelegatesPublicKeys.length,
+        //             true,
+        //         )} from a required ${maxDelegates}`,
+        //     );
+        // }
 
-        // Determine Next Forger Usernames...
-        this.logger.debug(
-            `Next Forgers: ${JSON.stringify(
-                nextForgers.slice(0, 5).map((publicKey: string) => this.getUsername(publicKey)),
-            )}`,
-        );
+        // // Determine Next Forger Usernames...
+        // this.logger.debug(
+        //     `Next Forgers: ${JSON.stringify(
+        //         nextForgers.slice(0, 5).map((publicKey: string) => this.getUsername(publicKey)),
+        //     )}`,
+        // );
 
-        const secondsToNextRound: number = (maxDelegates - forgingInfo.currentForger - 1) * blockTime;
+        // const secondsToNextRound: number = (maxDelegates - forgingInfo.currentForger - 1) * blockTime;
+        let forgingIndex = 2;
+        const finalList: any[] = [];
+        for (let i = 0; i < this.delegates.length; i++) {
+            const delegate = this.delegates[i];
 
-        for (const delegate of this.delegates) {
-            let indexInNextForgers = 0;
-            for (let i = 0; i < nextForgers.length; i++) {
-                if (nextForgers[i] === delegate.publicKey) {
-                    indexInNextForgers = i;
-                    break;
-                }
-            }
+            if (i === forgingInfo.nextForger) {
+                finalList.push({
+                    publicKey: delegate.publicKey,
+                    status: "next",
+                    time: 8000,
+                    order: i,
+                });
+            } else if (i > forgingInfo.nextForger) {
+                const nextTime = forgingIndex * 8000;
 
-            if (indexInNextForgers === 0) {
-                this.logger.debug(`${this.getUsername(delegate.publicKey)} will forge next.`);
-            } else if (indexInNextForgers <= maxDelegates - forgingInfo.nextForger) {
-                this.logger.debug(
-                    `${this.getUsername(delegate.publicKey)} will forge in ${Utils.prettyTime(
-                        indexInNextForgers * blockTime * 1000,
-                    )}.`,
-                );
+                forgingIndex++;
+
+                finalList.push({
+                    publicKey: delegate.publicKey,
+                    status: "pending",
+                    time: nextTime,
+                    order: i,
+                });
             } else {
-                this.logger.debug(`${this.getUsername(delegate.publicKey)} has already forged.`);
+                finalList.push({
+                    publicKey: delegate.publicKey,
+                    status: "done",
+                    time: 0,
+                    order: i,
+                });
             }
         }
 
-        this.logger.debug(`Round ${round.round} will end in ${Utils.prettyTime(secondsToNextRound * 1000)}.`);
+        // @ts-ignore
+        this.logger.warning(JSON.stringify({
+            method: 'activeDelegatesPublicKeys',
+            result: {
+                height,
+                timestamp,
+                round,
+                activeDelegatesPublicKeys,
+                finalList,
+            }
+        }, null, 4))
+
+        // for (const delegate of this.delegates) {
+        //     let indexInNextForgers = 0;
+        //     for (let i = 0; i < nextForgers.length; i++) {
+        //         if (nextForgers[i] === delegate.publicKey) {
+        //             indexInNextForgers = i;
+        //             break;
+        //         }
+        //     }
+
+        //     if (indexInNextForgers === 0) {
+        //         this.logger.debug(`${this.getUsername(delegate.publicKey)} will forge next.`);
+        //     } else if (indexInNextForgers <= maxDelegates - forgingInfo.nextForger) {
+        //         this.logger.debug(
+        //             `${this.getUsername(delegate.publicKey)} will forge in ${Utils.prettyTime(
+        //                 indexInNextForgers * blockTime * 1000,
+        //             )}.`,
+        //         );
+        //     } else {
+        //         this.logger.debug(`${this.getUsername(delegate.publicKey)} has already forged.`);
+        //     }
+        // }
+
+        // this.logger.debug(`Round ${round.round} will end in ${Utils.prettyTime(secondsToNextRound * 1000)}.`);
     }
 
-    /**
-     * @private
-     * @param {string} publicKey
-     * @returns {string}
-     * @memberof DelegateTracker
-     */
-    private getUsername(publicKey: string): string {
-        return this.walletRepository.findByPublicKey(publicKey).getAttribute("delegate.username");
-    }
+    // /**
+    //  * @private
+    //  * @param {string} publicKey
+    //  * @returns {string}
+    //  * @memberof DelegateTracker
+    //  */
+    // private getUsername(publicKey: string): string {
+    //     return this.walletRepository.findByPublicKey(publicKey).getAttribute("delegate.username");
+    // }
 }
